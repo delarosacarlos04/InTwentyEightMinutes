0c2465d767ccf06bf0b97c7339b00f866ed2e7c2
CDLR <delarosacarlos04@gmail.com>
2020-08-31 13:07:09 -0500

    new files

--- src/main/java/TwentyEightMinutes/Practice/Controller/LoginController.java
+++ src/main/java/TwentyEightMinutes/Practice/Controller/LoginController.java
@@ -3,16 +3,23 @@ package TwentyEightMinutes.Practice.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String loginMessage(@RequestParam String name, ModelMap model){
-        model.put("name", name);
-        System.out.println("name is" + name);
+
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model){
         return "login";
     }
+
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public String showWelcomePage(ModelMap model, @RequestParam String name){
+        model.put("name", name);
+        return "welcome";
+    }
+
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,10 +1,15 @@
 <html>
+
 <head>
 <title>First Web Application</title>
 </head>
 
 <body>
-My first JSP!! Welcome ${name}!
+	<form method="post">
+		Name : <input type="text" name="name" />
+		Password : <input type="password" name="password" />
+		<input type="submit" />
+	</form>
 </body>
 
 </html>
